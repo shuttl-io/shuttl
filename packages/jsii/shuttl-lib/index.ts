19c6638d1dcad197dd295f218230f3657f9072dc
Yoseph Radding <yoseph@shuttl.io>
2025-12-05 22:05:18 +0000

    [sync] feat: adds the shuttl jsii pacakge
    synced_from: 1164ee003e4e0d40e82b3b925b4c96d77753255e

--- packages/jsii/shuttl-lib/index.ts
+++ packages/jsii/shuttl-lib/index.ts
@@ -1,1 +1,15 @@
-export * from "./src";
+export interface GreeterProps {
+  readonly greetee: string;
+}
+
+export class Greeter {
+  private readonly greetee: string;
+
+  public constructor(props: GreeterProps) {
+    this.greetee = props.greetee;
+  }
+
+  public greet(): string {
+    return `Hello, ${this.greetee}!`
+  }
+}
