260f99a237cb5ac8f09ea871d2eb03031cfe13c9
Yoseph Radding <yoseph@shuttl.io>
2025-12-05 22:05:18 +0000

    [sync] feat: adds the shuttl jsii pacakge

---
+++ packages/jsii/shuttl-lib/src/index.ts
@@ -0,0 +1,15 @@
+export interface GreeterProps {
+  readonly greetee: string;
+}
+
+export class Greeter {
+  private readonly greetee: string;
+
+  public constructor(props: GreeterProps) {
+    this.greetee = props.greetee;
+  }
+
+  public greet(): string {
+    return `Hello, ${this.greetee}!`
+  }
+}
